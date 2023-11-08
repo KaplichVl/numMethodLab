3427f7046f06ad9304c01118b7b12ef19ec8c30f
KaplichVl <vladimirnom8@gmail.com>
2023-11-08 22:39:58 +0300

    init code base: newton

---
+++ Newton/Newton/Newton.cpp
@@ -0,0 +1,160 @@
+#include <iomanip>
+#include <iostream>
+#include "Newton.h"
+
+
+
+
+double func1(double x1, double x2)
+{
+	return 1.5 * x1 * x1 * x1 - x2 * x2 - 1;
+}
+
+double func2(double x1, double x2)
+{
+	return x1 * x2 * x2 * x2 - x2 - 4;
+}
+
+vector<vector<double>> jacobi(double x1, double x2)
+{
+	vector <vector<double>> jacob = { {4.5 * x1 * x1,-2 * x2},
+		           {x2 * x2 * x2,3 * x1 * x2 * x2 - 1} };
+    return jacob;
+}
+
+vector<vector<double>> jacobi(double x1,double x2, double M)
+{
+    vector<vector<double>> J = { {(func1(x1 + M * x1, x2) - func1(x1, x2)) / (M * x1),(func1(x1, x2 + M * x2) - func1(x1, x2)) / (M * x2)} , 
+                                 {(func2(x1 + M * x1, x2) - func2(x1, x2)) / (M * x1),(func2(x1, x2 + M * x2) - func2(x1, x2)) / (M * x2)} };
+    return J;
+}
+
+vector<double> gauss(vector<vector<double>> a, vector<double> y)
+{
+	int n = y.size();
+    vector<double> c(0);
+    vector<double> x(n);
+    double max;
+    int k, index;
+    k = 0;
+    while (k < n) {
+        max = a[k][k];
+        index = k;
+        for (int i = k + 1; i < n; i++) {
+            if (a[i][k] > max) {
+                max = a[i][k];
+                index = i;
+            }
+        }
+        if (k != index) {
+            swap(a[k], a[index]);
+            swap(y[k], y[index]);
+        }
+
+        for (int i = k; i < n; i++) {
+            double temp = a[i][k];
+            for (int j = 0; j < n; j++)
+                a[i][j] /= temp;
+            y[i] /= temp;
+            if (i == k)  continue;
+            for (int j = 0; j < n; j++)
+                a[i][j] -= a[k][j];
+            y[i] -= y[k];
+        }
+        k++;
+    }
+
+    for (k = n - 1; k >= 0; k--) {
+        x[k] = y[k];
+        for (int i = 0; i < k; i++)
+            y[i] = y[i] - a[i][k] * x[k];
+    }
+    return x;
+}
+
+vector<double> newtonMethod(vector <double> x1)
+{
+        vector <double> dx{};
+        int cnt = 0 ,n = x1.size();
+        double delta_1 = 0, delta_2 = 0;
+    
+        cout << "Iteration " << setw(12) << "delta_1 " << setw(15)<< "delta_2 " << endl;
+        do {
+            double func_1 = func1(x1[0],x1[1]);
+            double func_2 = func2(x1[0],x1[1]);
+    
+            vector<double> temp = { -1 * func_1, -1 * func_2};
+    
+            dx = gauss(jacobi(x1[0],x1[1]), temp);
+    
+            delta_1 = max(func_1, func_2);
+    
+            for (int i = 0; i < n; i++) {
+                x1[i] += dx[i];
+            }
+    
+            delta_2 = 0;
+            for (int i = 0; i < n; i++) {
+                if (abs(x1[i]) < 1)
+                    delta_2 = max(delta_2, abs(dx[i]));
+    
+                else
+                    delta_2 = max(delta_2, abs(dx[i]) / x1[i]);
+    
+            }
+    
+            cnt++;
+            cout << cnt << setw(20) << delta_1 << setw(16)  << delta_2 << endl;
+        } while (abs(delta_1) > epsilon || abs(delta_2) > epsilon);
+    
+       
+        return x1;
+}
+
+vector<double> newtonMethod(vector <double> x1, int NIT, double M) {
+    int iter = 1;
+    vector<vector<double>> J;
+    vector<double> dx(2), x_k(2), F(2);
+    double delta_1 =0 , delta_2 = 0,temp;
+    cout << left << setw(4) << "iter    " << setw(20) << "delta_1" << setw(20) << "delta_2" << setw(20) << "x1" << setw(20) << "x2" << endl;
+    do
+    {
+        F[0] = -1 * func1(x1[0],x1[1]);
+        F[1] = -1 * func2(x1[0], x1[1]);
+        J = jacobi(x1[0], x1[1]);
+        dx = gauss(J, F);
+        x_k[0] = x1[0] + dx[0];
+        x_k[1] = x1[1] + dx[1];
+
+        delta_1 = max(abs(F[0]),abs(F[1]));
+        delta_2 = 0;
+        for (int i = 0; i < x1.size(); i++) {
+            if (abs(x1[i]) < 1)
+                delta_2 = max(delta_2, abs(dx[i]));
+
+            else
+                delta_2 = max(delta_2, abs(dx[i]) / x1[i]);
+
+        }
+       x1[0] = x_k[0];
+       x1[1] = x_k[1];
+       cout << left << setw(8) << iter << setw(20) << delta_1 << setw(20) << delta_2 << setw(20) << x1[0] << setw(20) << x1[1] << endl;
+       iter++;
+       if (iter > NIT)
+           exit(1);
+
+    } while (delta_1 > epsilon && delta_2 > epsilon);
+
+
+       return x1;
+}
+
+
+
+void printRes(const vector<double>& res)
+{
+	cout << "Resolution : " << endl;
+	    for (double elem : res)
+	        cout << elem << " ";
+       
+}
---
+++ Newton/Newton/main.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+#include "Newton.h"
+
+
+int main()
+{
+    vector<double> x0 = { 1,1 };
+    cout << endl << "Method number 1: " << endl << endl;
+
+    printRes(newtonMethod(x0));
+    cout <<endl << "Method number 2: M = 0.01 " << endl << endl;
+
+    printRes(newtonMethod(x0,10,0.01));
+    cout << endl << "Method number 3: M = 0.05 " << endl << endl;
+
+    printRes(newtonMethod(x0, 10, 0.05));
+    cout << endl << "Method number 4: M = 0.1 " << endl << endl;
+    printRes(newtonMethod(x0, 10, 0.1));
+
+}
+
